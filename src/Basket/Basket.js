bba8551946c7c609ef39014952d329c0e34fc212
aaron foster bresley <afbresley@gmail.com>
2017-12-10 13:51:21 -0500

    getting scss set up, and come dummy components

---
+++ src/Basket/Basket.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './basket.css';
+
+class Basket extends Component {
+
+  render() {
+    return (
+      <div className="basketContainer">
+        <p>
+          this is a basket
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Basket;
